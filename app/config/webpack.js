b47a6967c8496d3e92f100add90eb9fb0470ccf3
David Schissler <david.schissler@gmail.com>
2018-02-16 12:45:11 -0800

    Add Webpack 4 default mode.

--- app/config/webpack.js
+++ app/config/webpack.js
@@ -2,13 +2,14 @@
 const path = require('path')
 const webpackConfig = require('perch-framework/webpack-config')
 
-const {APP_DIR, PACKAGE_DIR} = process.env
+const {APP_DIR, PACKAGE_DIR, WEBPACK_MODE} = process.env
 
 /**
  *
  */
 module.exports = webpackConfig({
   appDir: APP_DIR,
+  mode: WEBPACK_MODE,
   nodeModulesDir: path.join(PACKAGE_DIR, 'node_modules'),
 })
 
