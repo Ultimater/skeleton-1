6ee332131188292137639f5abfc9aeb97aaa5247
David Schissler <david.schissler@gmail.com>
2018-02-25 19:38:00 -0800

    Redesign application setup.

--- app/services/common/config.php
+++ app/services/common/config.php
@@ -3,5 +3,5 @@ use Perch\Application\AutoApp\Config as AutoAppConfig;
 use Phalcon\Di\Service\SharedService;
 
 return new SharedService(function() {
-    return require AutoAppConfig::getDefault()->path->configDir . '/config.php';
+    return AutoAppConfig::getConfig();
 });
