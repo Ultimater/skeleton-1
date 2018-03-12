4f27538527a2ae6fded92f10e0e33634861f284a
David Schissler <david.schissler@gmail.com>
2018-03-11 23:13:55 -0700

    Add devServer section to webpack section.

--- app/config/webpack.js
+++ app/config/webpack.js
@@ -1,12 +1,14 @@
-const webpackConfig = require('perch-framework/webpack-config')
+const webpackConfig = require('perch-framework')
 
-const {APP_DIR, NODE_PATH} = process.env
+const {APP_DIR, TMP_DIR, NODE_PATH} = process.env
 
 /**
  *
  */
 module.exports = webpackConfig({
-  appDir: APP_DIR,
+  configDir: `${APP_DIR}config/`,
+  webpackDir: `${APP_DIR}webpack/`,
+  tmpDir: TMP_DIR,
   nodeModulesDir: NODE_PATH,
 })
 
