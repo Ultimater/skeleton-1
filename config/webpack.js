ef23f505e07e9ca4d5d3051bff6849a9168cb571
David Schissler <david.schissler@gmail.com>
2018-04-21 13:49:21 -0700

    Improved webpack config.

--- config/webpack.js
+++ config/webpack.js
@@ -1,27 +1,17 @@
-const {createEntryMap, createConstants, devServer, getAppConfig} = require('perch-core')
-
-// All five core paths and the target environment are automatically exported.
-const {ROOT_DIR, APP_DIR, CONFIG_DIR, TMP_DIR, ENV_DIR, ENV} = process.env
+const {createAppConfig, createConstants, createEntryMap, devServer} = require('@perch-framework/core')
 
 // All user defined variables are automatically exported from files ".env" and ".env.ENV".
 const {YOUR_VARIABLE} = process.env
 
-// Setup necessary paths.
-const webpackRootDir = `${APP_DIR}/webpack/`
-const modulesDir = `${APP_DIR}webpack/modules/`
-const assetDir = `${APP_DIR}/assets`
-const nodeModulesDir = `${ROOT_DIR}node_modules/`
-const tmpDir = `${TMP_DIR}/webpack`
-
-// Get the webpack section of the dev config.
-const config = require(`${CONFIG_DIR}dev`).webpack
+const config = createAppConfig()
+const {assetDir, modulesDir, vendorModulesDir, rootDir, tmpDir, webpackDir} = config
 
 /**
  *
  */
 module.exports = {
-  context: webpackRootDir,
-  entry: createEntryMap(webpackRootDir),
+  context: webpackDir,
+  entry: createEntryMap(webpackDir),
   output: {
     path         : '/TODO',
     publicPath   : 'js/',
@@ -29,7 +19,7 @@ module.exports = {
   resolve: {
     modules: [
       modulesDir,
-      nodeModulesDir,
+      vendorModulesDir,
     ],
     extensions: [
       '.js',
@@ -47,24 +37,20 @@ module.exports = {
         test: /\.css$/,
         exclude: /node_modules/,
         use: [
-          {
-            loader: 'style-loader',
-          },
+          'style-loader',
           {
             loader: 'css-loader',
             options: {
               importLoaders: 1,
             },
           },
-          {
-            loader: 'postcss-loader',
-          },
+          'postcss-loader',
         ],
       },
     ],
   },
   devServer: devServer({}),
   plugins: [
-    createConstants(ENV, ROOT_DIR, config.constants),
+    createConstants(config),
   ],
 }
