bf79926c207d7c1ceb3fa97504b7ef1041dfeb51
David Schissler <david.schissler@gmail.com>
2018-05-06 21:16:16 -0700

    Use new service format in runmode configs.

---
+++ app/services/CliServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+use Phalcon\DiInterface;
+use Phalcon\Di\ServiceProviderInterface;
+
+/**
+ *
+ */
+class CliServiceProvider implements ServiceProviderInterface
+{
+
+    /**
+     *
+     */
+    public function register(DiInterface $di)
+    {
+    }
+}
